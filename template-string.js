1c86e513a4cee92aa26bc1145bc10a9d8133e1bb
Sunny <shubelislam.diu@gmail.com>
2021-08-23 18:34:22 +0600

    template parameter

---
+++ template-string.js
@@ -0,0 +1,13 @@
+const student1 = 'Omor';
+const student2 = "Faruk";
+const student3 = `Ashraful`;
+
+const multiLine = `This is a paragraph.
+Here is the second line.
+And it is the third line.`;
+console.log(multiLine);
+
+const firstName = 'Mamun';
+const lastName = 'Islam';
+const fullName = `${firstName} ${lastName}`
+console.log(fullName);
