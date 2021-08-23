9963370a4c8d91750e7339d4c7eb5d09ba861e38
Sunny <shubelislam.diu@gmail.com>
2021-08-23 19:22:42 +0600

    arrow function

---
+++ arrow-function.js
@@ -0,0 +1,25 @@
+// normal function expression
+function add1 (num1, num2) {
+    return num1 + num2;
+}
+const sum1 = add1(15, 17);
+console.log(sum1);
+
+// function stored in variable
+const add2 = function add (num1, num2) {
+    return num1 + num2;
+}
+const sum2 = add2(15, 17);
+console.log(sum2);
+
+// anonymous function
+const add3 = function (num1, num2) {
+    return num1 + num2;
+}
+const sum3 = add3(15, 17);
+console.log(sum3);
+
+// arrow function
+const add4 = (num1, num2) => num1 + num2;
+const sum4 = add4(15, 17);
+console.log(sum4);
