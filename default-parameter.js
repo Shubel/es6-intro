725610dbb11606f4d67f8eba7b53c6edf1d8e564
Sunny <shubelislam.diu@gmail.com>
2021-08-23 19:05:41 +0600

    number added

--- default-parameter.js
+++ default-parameter.js
@@ -4,3 +4,9 @@ function fullName (first, last = 'Islam') {
 }
 const student = fullName('Aminul');
 console.log(student);
+
+// add number
+function add (num1, num2 = 10) {
+    return num1 + num2;
+}
+console.log(add(15));
