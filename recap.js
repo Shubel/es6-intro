67cdde1d6bf992ff77c70a67d3a463d0d21faeb4
Sunny <shubelislam.diu@gmail.com>
2021-08-25 09:06:39 +0600

    recap

---
+++ recap.js
@@ -0,0 +1,25 @@
+// array length, push, pop
+const array =[2, 4, 6, 8, 10 , 12];
+const arrayLength = array.length;
+console.log(arrayLength);
+
+array.push(84, 89);
+console.log(array);
+
+array.pop();
+console.log(array);
+
+array[2] = 456;
+console.log(array);
+
+// includes()
+const array = [1,2,3,4,5,6];
+if(array.includes(4)) {
+    console.log("true 4 was found in the array") // true 4 was found in the array
+    };
+
+// indexOf()
+const array = [1,2,3,4,5,6];
+if(array.indexOf(4) > -1 ) {
+    console.log("true 4 was found in the array") // true 4 was found in the array
+    }
